998e7c4419f269d0d816d0d1ee82884a020d2a1d
Jonal <jobn@stanford.edu>
2018-03-04 01:03:01 +0000

    added files from milestone

---
+++ createSpectrograms.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import argparse
+import tqdm as tqdm  #For progress bars etc
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--audio_dir', default='data/wav_songs', help="Directory with the .wav SONGS dataset")
+parser.add_argument('--output_dir', default='data/raw_specs', help="Where to write the new data")
+pixpersec = 50
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+
+	audio_dir = args.audio_dir   # source of the .wav songs
+	specs_dir = args.output_dir  # Output directory for the raw spectrograms
+
+	assert os.path.isdir(audio_dir), "Couldn't find the dataset at {}".format(audio_dir)
+
+	if not os.path.exists(specs_dir):
+		os.mkdir(specs_dir)
+	else:
+		print("Warning: output dir {} already exists".format(specs_dir))
+
+
+	for fname in tqdm(os.listdir(audio_dir)):
+		if fname.endswith(".wav"):
+			source = os.path.join(audio_dir, fname)
+			dest = os.path.join(specs_dir, str(fname.split(".")[0])+".png")
+			"""
+			Create a command for the os to run sox and create a spectrogram
+			remix 1 selects the left channel of the music
+			-Y 200 sets the y axis to be 200 pixels
+			-X pixpersec sets the number of pixels per second in the spectrogram
+			-m for grayscale
+			-r Get a raw spectrogram without axes
+			-o output file
+			"""
+
+			command = ['sox', source, '-n', 'remix', '1','spectrogram', '-Y', '200', '-X', str(pixpersec), '-m', '-r', '-o', dest]
+			subprocess.call(command)
+
+
+
+
+
+
