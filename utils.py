ed100fdb78e7b962f6dc2d47674a85f5d30b979a
Ubuntu <ubuntu@ip-172-31-46-240.us-west-2.compute.internal>
2018-03-18 10:53:22 +0000

    commit after modifying plot_conf_matrix

--- utils.py
+++ utils.py
@@ -8,6 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sliceSpecs import slice_spec
 
+from sklearn.metrics import confusion_matrix
+import itertools
+
 import torch
 
 
@@ -151,30 +154,81 @@ def load_checkpoint(checkpoint, model, optimizer=None):
 
     return checkpoint
 
+def plot_confusion_matrix_2(cm, classes,
+                          normalize=False,
+                          save_plot = False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
-def plot_confusion_matrix(y_actu, y_pred, save_plot, title='Confusion matrix', cmap=plt.cm.gray_r):
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    if save_plot:
+        plt.savefig("conf_matrix_plot.svg")
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
     
+    
+def plot_confusion_matrix(y_actu, y_pred, save_plot, title='Confusion matrix', cmap=plt.cm.gray_r):
     y_actu = y_actu.reshape((y_actu.shape[1], ))
     y_pred = y_pred.reshape((y_pred.shape[1], ))
     
-    df_confusion = pd.crosstab(y_actu, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
+    cnf_matrix = confusion_matrix(y_actu, y_pred);
     
-    df_conf_norm = df_confusion / df_confusion.sum(axis=1)
-    print(df_confusion)
+    np.set_printoptions(precision=2)
     
-    plt.matshow(df_confusion, cmap=cmap) # imshow
-    #plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(df_confusion.columns))
-    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
-    plt.yticks(tick_marks, df_confusion.index)
-    if save_plot:
-        plt.tight_layout()
-        plt.savefig("conf_matrix_plot.svg")
-        
-    plt.ylabel(df_confusion.index.name)
-    plt.xlabel(df_confusion.columns.name)
+    class_names = np.array(['Elect.', 'Expertl', 'Folk', 'Hip-Hop', 'Instrl', 'Intnl', 'Pop', 'Rock'])
+    
+    plt.figure()
+    plot_confusion_matrix_2(cnf_matrix, classes=class_names, save_plot = save_plot, normalize = True)
     plt.show()
+    
+#     y_actu = y_actu.reshape((y_actu.shape[1], ))
+#     y_pred = y_pred.reshape((y_pred.shape[1], ))
+    
+#     df_confusion = pd.crosstab(y_actu, y_pred, rownames=['Actual'], colnames=['Predicted'], margins=True)
+    
+#     df_conf_norm = df_confusion / df_confusion.sum(axis=1)
+#     print(df_confusion)
+    
+#     plt.matshow(df_confusion, cmap=cmap) # imshow
+#     #plt.title(title)
+#     plt.colorbar()
+#     tick_marks = np.arange(len(df_confusion.columns))
+#     plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+#     plt.yticks(tick_marks, df_confusion.index)
+#     if save_plot:
+#         plt.tight_layout()
+#         plt.savefig("conf_matrix_plot.svg")
+        
+#     plt.ylabel(df_confusion.index.name)
+#     plt.xlabel(df_confusion.columns.name)
+#     plt.show()
+
+
     
     
 def plot_bar_graph(genre_confidences):
